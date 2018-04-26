0d7d10c5cb4dae3ba3e7eb98136fa975addb02ab
kepyor <kepyor@gmail.com>
2018-04-26 00:55:25 -0500

    commit

--- SysCliVet/SysCliVet/Privado/PaginaMaestra/Inicio.Master.designer.cs
+++ SysCliVet/SysCliVet/Privado/PaginaMaestra/Inicio.Master.designer.cs
@@ -11,5 +11,14 @@ namespace SysCliVet.Privado.PaginaMaestra {
     
     
     public partial class Inicio {
+        
+        /// <summary>
+        /// Control ContentPlaceHolder1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
