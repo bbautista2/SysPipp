17639a7c24290c4e1e61fab2540b692aec156331
Betsy Bautista <bbautista.ortiz@hotmail.com>
2018-06-09 01:10:02 -0500

    commit fichaclinica sin validaciones

--- SysCliVet/CapaDatos/clsFichaClinicaDAO.cs
+++ SysCliVet/CapaDatos/clsFichaClinicaDAO.cs
@@ -28,7 +28,6 @@ namespace CapaDatos
 
         public Boolean Guardar(ref clsBaseEntidad baseEntidad, clsFichaClinica objFichaClinica)
         {
-
             Boolean Resultado = false;
             SqlCommand cmd = null;
             try
@@ -39,14 +38,38 @@ namespace CapaDatos
                 };
                 if (objFichaClinica.ListaVacunas.Count > 0)
                     cmd.Parameters.Add(new SqlParameter { ParameterName = "@TYPE_VACUNAS", Value = objFichaClinica.ListaVacunas, SqlDbType = SqlDbType.Structured, TypeName = "dbo.TY_VACUNAS" });
-                cmd.Parameters.AddWithValue("@ID", objPropietario.Id);
-                cmd.Parameters.AddWithValue("@Nombre", objPropietario.Nombre);
-                cmd.Parameters.AddWithValue("@Apellidos", objPropietario.Apellidos);
-                cmd.Parameters.AddWithValue("@FechaNacimiento", objPropietario.FechaNacimiento);
-                cmd.Parameters.AddWithValue("@Direccion", objPropietario.Direccion);
-                cmd.Parameters.AddWithValue("@Telefono", objPropietario.Telefono);
-                cmd.Parameters.AddWithValue("@Email", objPropietario.Email);
-                cmd.Parameters.AddWithValue("@Estado", objPropietario.Estado);
+                //Datos Ficha Clínica
+                cmd.Parameters.AddWithValue("@ID", objFichaClinica.Id);
+                cmd.Parameters.AddWithValue("@InformacionMedica", objFichaClinica.InformacionMedica);
+                cmd.Parameters.AddWithValue("@MedioAmbiente", objFichaClinica.MedioAmbiente);
+                cmd.Parameters.AddWithValue("@TipoDieta", objFichaClinica.TipoDieta);
+                cmd.Parameters.AddWithValue("@Motivo", objFichaClinica.Motivo);
+                cmd.Parameters.AddWithValue("@Observaciones", objFichaClinica.Observaciones);
+                cmd.Parameters.AddWithValue("@Estado", objFichaClinica.Estado);
+                //Datos Propietario
+                cmd.Parameters.AddWithValue("@ProiId", objFichaClinica.Propietario.Id);
+                cmd.Parameters.AddWithValue("@ProiDni", objFichaClinica.Propietario.Dni);
+                cmd.Parameters.AddWithValue("@ProNombre", objFichaClinica.Propietario.Nombre);
+                cmd.Parameters.AddWithValue("@ProApellidos", objFichaClinica.Propietario.Apellidos);
+                cmd.Parameters.AddWithValue("@ProFechaNacimiento", objFichaClinica.Propietario.FechaNacimiento);
+                cmd.Parameters.AddWithValue("@ProDireccion", objFichaClinica.Propietario.Direccion);
+                cmd.Parameters.AddWithValue("@ProTelefono", objFichaClinica.Propietario.Telefono);
+                cmd.Parameters.AddWithValue("@ProEmail", objFichaClinica.Propietario.Email);
+                cmd.Parameters.AddWithValue("@ProEstado", objFichaClinica.Propietario.Estado);
+                //Datos Mascota
+                cmd.Parameters.AddWithValue("@MasId", objFichaClinica.Mascota.Id);
+                cmd.Parameters.AddWithValue("@MasNombre", objFichaClinica.Mascota.Nombre);
+                cmd.Parameters.AddWithValue("@MasFechaNacimiento", objFichaClinica.Mascota.FechaNacimiento);
+                cmd.Parameters.AddWithValue("@MasRaza", objFichaClinica.Mascota.Raza);
+                cmd.Parameters.AddWithValue("@MasColor", objFichaClinica.Mascota.Color);
+                cmd.Parameters.AddWithValue("@MasEspecie", objFichaClinica.Mascota.Especie);
+                cmd.Parameters.AddWithValue("@MasSexo", objFichaClinica.Mascota.Sexo);
+                cmd.Parameters.AddWithValue("@MasIntac", objFichaClinica.Mascota.Intac);
+                cmd.Parameters.AddWithValue("@MasCast", objFichaClinica.Mascota.Cast);
+                cmd.Parameters.AddWithValue("@MasPeso", objFichaClinica.Mascota.Peso);
+                cmd.Parameters.AddWithValue("@MasMarcaDistintiva", objFichaClinica.Mascota.MarcaDistintiva);
+                cmd.Parameters.AddWithValue("@MasEstado", objFichaClinica.Mascota.Estado);
+                
                 cmd.ExecuteReader();
                 Resultado = true;
             }
--- SysCliVet/SysCliVet/Privado/FichaClinica/Guardar.aspx
+++ SysCliVet/SysCliVet/Privado/FichaClinica/Guardar.aspx
@@ -77,6 +77,14 @@
                             </div>
                             <div class="item form-group">
                                 <label class="control-label col-md-3 col-sm-3 col-xs-12">
+                                    Apellidos <span class="required">*</span>
+                                </label>
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                    <input id="txtApellidos" runat="server" class="form-control col-md-7 col-xs-12" name="txtApellidos" required="required" type="text">
+                                </div>
+                            </div>
+                            <div class="item form-group">
+                                <label class="control-label col-md-3 col-sm-3 col-xs-12">
                                     Fecha Nac <span class="required">*</span>
                                 </label>
                                 <div class="col-md-5 col-sm-6 col-xs-12">
